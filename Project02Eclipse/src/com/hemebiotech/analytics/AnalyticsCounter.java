5807cb4c1df1c5c097288f812f28217851b95675
yoann diouy <ydiouy@gmail.com>
2024-10-24 22:17:35 +0200

    ‘begin”

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,61 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
+	private static int headacheCount = 0;
+	private static int rashCount = 0;
+	private static int pupilCount = 0;
+        private static int symptomNbr = 0;
+
 	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+            //final String FILE_SYMPTOMS_PATH = "C:\\Users\\ydiouy\\formation_java\\2_debug_java\\Project02Eclipse\\symptoms.txt";
+            final String FILE_SYMPTOMS_PATH = "ressources/symptoms.txt";
+            
+            Map<String,Integer> symptoms = new HashMap<String,Integer>();
+
+            try {
+                // first get input
+                FileReader fileReader = new FileReader(FILE_SYMPTOMS_PATH);
+		BufferedReader reader = new BufferedReader (fileReader);
 		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
+		int i = 0;
+		int headCount = 0;
 		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+                    System.out.println(line);
+                    i++;
+                    System.out.println("symptom from file: " + line);
 
-			line = reader.readLine();	// get another symptom
+                    if (symptoms.get(line) != null) {
+                        int count = symptoms.get(line);
+                        symptoms.put(line, count);
+                    }
+
+                    line = reader.readLine();
 		}
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+            try {
+                // next generate output
+                FileWriter writer = new FileWriter ("result.out");
+                BufferedWriter bufferedwriter = new BufferedWriter(writer);
+                
+                // Boucler sur symptoms pour afficher le nombre de chaques symptomes réferencés
+                bufferedwriter.write("headache: " + headacheCount + "\n");
+                bufferedwriter.write("rash: " + rashCount + "\n");
+                bufferedwriter.write("dialated pupils: " + pupilCount + "\n");
+                bufferedwriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 	}
 }
